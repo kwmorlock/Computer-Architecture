fe26084e7863cc1efbbd663fe1b8d9a9142857f2
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-07-21 16:18:01 -0400

    cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # pass
+        #initialize
+        self.ram = [0] * 256 #bytes of memory (256)
+        self.reg = [0] * 8 # amount of registers that store data (8)
+        self.pc = 0 #the counter, sorta like a pointer
+
+    def ram_read(self, MAR): #MAR is memory address register holds memory / position
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR): #MAR memory data register is data getting written into MAR LS8.spec file
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +72,55 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # pass
+
+        running = True
+
+        instructions = {
+            0b10000010: 'LDI', # LDI R0,8
+            0b01000111: 'PRN', # PRN R0
+            0b00000001: 'HLT', # HLT
+        }
+
+        while running:
+            i = self.ram[self.pc] #grabbing the index from ram(memory)from comp.py notepad
+            
+            if instructions[i] == 'LDI':
+                reg_num = self.ram[self.pc + 1] #set up register
+                value = self.ram[self.pc + 2] #what does this mean from guided project??
+                self.reg[reg_num] = value #save value to register
+
+                self.pc +=3
+
+            elif instructions[i] == 'PRN':
+                #print register
+                reg_num = self.ram[self.pc + 1]
+                print(self.reg[reg_num])
+
+                self.pc +=2
+
+            elif instructions[i] == 'HLT':
+                #this is the halt
+                running = False
+
+            else:
+                print(f"Unknown instruction {i}")
+
+
+
+
+
+
+
+
+new_cpu = CPU()
+new_cpu.load()
+new_cpu.run()
+
+
+#ls8-spec and training kit
+#ldi registers
+#hlt alts cpu exists emulator
+
+#ram read and write in TK
+
